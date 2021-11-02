f6e507f201fc87440fcaaebfc07aedec6b33456e
Aureliano <81753529+Aureliano90@users.noreply.github.com>
2021-11-02 04:20:53 -0500

    Delete okex-node-sdk-api directory

--- okex-node-sdk-api/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/PublicClient';
-export * from './lib/AuthenticatedClient';
-export * from './lib/WebsocketClient';
--- okex-node-sdk-api/src/lib/PublicClient.spec.ts
+++
@@ -1,6 +0,0 @@
-// tslint:disable:no-expression-statement
-import test from 'ava';
-
-test('getSpotInstruments', async t => {
-  t.is(true, true);
-});
